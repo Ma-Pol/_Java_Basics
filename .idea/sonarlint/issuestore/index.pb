23b9f573f1f0573e5c762e001094e3c858b849db
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-10-29 16:14:24 +0900

    기본형 타입

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,7 @@
 
 J
 src/part01/HelloWorld.java,4\3\430217cec1684315af1339a378cfd61a3eed29f5
-D
-src/part01/Main.java,6\f\6f868d065e261618f04c25dd5a4d0e4a2e9b51d3
+O
+src/part02/VariableExample.java,0\1\0141b177114cb62ab982745439e461925230fb40
+O
+src/part02/ConstantExample.java,1\6\16e9b04cc2f70ad6d48be3d1d4fc35472cbb0868
---
+++ src/part02/PrimitiveDataTypeExample.java
@@ -0,0 +1,23 @@
+package part02;
+
+public class PrimitiveDataTypeExample {
+	public static void main(String[] args) {
+		boolean isFun = true; // true or false
+		System.out.println("isFun = " + isFun);
+
+		char c = 'A'; // 하나의 문자
+		System.out.println("c = " + c);
+
+		int x = 59; // 정수
+		System.out.println("x = " + x);
+
+		long big = 3456789l; // 큰 정수
+		System.out.println("big = " + big);
+
+		float f = 3.14f; // 작은 실수
+		System.out.println("f = " + f);
+
+		double d = 3.14159265358979323846; // 큰 실수
+		System.out.println("d = " + d);
+	}
+}
