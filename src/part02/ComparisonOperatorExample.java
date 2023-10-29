ddf6eff3fa63908d803e91714912fce994c5dd75
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-10-29 22:35:16 +0900

    비교 연산자

---
+++ src/part02/ComparisonOperatorExample.java
@@ -0,0 +1,22 @@
+package part02;
+
+public class ComparisonOperatorExample {
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 10;
+
+		System.out.println("i == j : " + (i == j)); // true
+		System.out.println("i != j : " + (i != j)); // false
+		System.out.println("i < j : " + (i < j)); // false
+		System.out.println("i <= j : " + (i <= j)); // true
+		System.out.println("i > j : " + (i > j)); // false
+		System.out.println("i >= j : " + (i >= j)); // true
+
+		i += 10; // i = i + 10;
+
+		System.out.println(i);
+
+		System.out.println(i -= 5);
+		System.out.println(i);
+	}
+}
