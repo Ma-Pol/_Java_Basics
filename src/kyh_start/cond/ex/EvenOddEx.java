8215c97aebc590100ad15952a51fabaffa1e107d
MaPole <ysh.skyblue6485@gmail.com>
2024-08-08 22:18:21 +0900

    [김영한의 자바 입문] 04 조건문

---
+++ src/kyh_start/cond/ex/EvenOddEx.java
@@ -0,0 +1,10 @@
+package kyh_start.cond.ex;
+
+public class EvenOddEx {
+    public static void main(String[] args) {
+        int x = 2;
+        String evenOdd = (x % 2 == 0) ? "짝수" : "홀수";
+
+        System.out.println("x = " + x + ", " + evenOdd);
+    }
+}
