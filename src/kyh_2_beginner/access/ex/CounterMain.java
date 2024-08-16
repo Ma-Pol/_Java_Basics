ba958ae8084d172a2269a4992f7f60bc4c6f4f9b
MaPole <ysh.skyblue6485@gmail.com>
2024-08-17 03:35:14 +0900

    [김영한의 자바 기본] 06 접근제어자

---
+++ src/kyh_2_beginner/access/ex/CounterMain.java
@@ -0,0 +1,13 @@
+package kyh_2_beginner.access.ex;
+
+public class CounterMain {
+    public static void main(String[] args) {
+        MaxCounter counter = new MaxCounter(3);
+        counter.increment();
+        counter.increment();
+        counter.increment();
+        counter.increment();
+        int count = counter.getCount();
+        System.out.println(count);
+    }
+}
