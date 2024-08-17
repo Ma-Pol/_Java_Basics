d317795e2fb99fbf222d75130ede267bee766031
MaPole <ysh.skyblue6485@gmail.com>
2024-08-18 08:08:38 +0900

    [김영한의 자바 기본] 08 final

---
+++ src/kyh_2_beginner/final1/Data.java
@@ -0,0 +1,5 @@
+package kyh_2_beginner.final1;
+
+public class Data {
+    public int value; // 변경 가능
+}
---
+++ src/kyh_2_beginner/final1/ex/MemberMain.java
@@ -0,0 +1,10 @@
+package kyh_2_beginner.final1.ex;
+
+public class MemberMain {
+    public static void main(String[] args) {
+        Member member = new Member("myId", "kim");
+        member.print();
+        member.changeData("myId2", "seo");
+        member.print();
+    }
+}
